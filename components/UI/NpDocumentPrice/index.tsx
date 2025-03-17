990a7a4cd04b35be43379f4d466014919141e4bc
Roman Lysyk <r.lysyk@synchronic.info>
2025-03-17 13:32:47 +0200

    fix delivery calculator

--- components/UI/NpDocumentPrice/index.tsx
+++ components/UI/NpDocumentPrice/index.tsx
@@ -20,6 +20,9 @@ const NpDocumentPrice: FC<NpDocumentPriceProps> = ({ offer_id, quantity, price }
 	const { city } = useAppSelector(state => state.orderReducer);
 	const { data, isLoading } = baseDataAPI.useFetchNpDocumentPriceQuery({ offer_id, ref: city.value, count: quantity });
 	const t = useTranslations('Delivery');
+	const num = (e: number) => {
+		return (e % 1) === 0 ? e.toFixed(0) : e.toFixed(2);
+	}
 
 	return <Spinner height='h-40' show={ isLoading }>
 		<p className='mt-4'>
@@ -30,7 +33,7 @@ const NpDocumentPrice: FC<NpDocumentPriceProps> = ({ offer_id, quantity, price }
 		</h3>
 		<h3 className="text-base font-semibold leading-6 text-gray-900 mt-3">
 			{ t('with cash') + ': ' }
-			{ Math.round(price * quantity * postpaid.cof * 100) / 100 + postpaid.const + data?.[0].Cost } грн
+			{ num(price * quantity * postpaid.cof + postpaid.const + data?.[0].Cost) } грн
 		</h3>
 	</Spinner>
 };
